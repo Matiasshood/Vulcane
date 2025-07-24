1185aa992c65a67b8e23156de565b83a33a55784
Matiasshood <Matiase.marchi@gmail.com>
2025-07-24 00:58:22 -0300

    Add files via upload

--- js/productos.js
+++ js/productos.js
@@ -1,53 +1,53 @@
 
-
+const basePath = window.location.pathname.includes("/pages/") ? "../image/" : "image/";
 let productos = [
     {   
         id: "01",
         nombre: "Hoodie",
         Destacado: true,
-        imagen: "../image/buzo1.png",
+        imagen: basePath + "buzo1.png",
         precio: 30000
     },
     {   
         id: "02",
         nombre: "Hoodie Sky",
         Destacado: false,
-        imagen: "../image/buzo2.png",
+        imagen: basePath + "buzo2.png",
         precio: 30000
     },
     {   
         id: "03",
         nombre: "Hoodie Logo",
         Destacado: true,
-        imagen: "../image/buzo3.png",
+        imagen: basePath + "buzo3.png",
         precio: 28000
     },
     {   
         id: "04",
         nombre: "Jean Black",
         Destacado: false,
-        imagen: "../image/jean.png",
+        imagen: basePath + "jean.png",
         precio: 65000
     },
     {   
         id: "05",
         nombre: "Shirt Lava",
         Destacado: false,
-        imagen: "../image/remera1.png",
+        imagen: basePath + "remera1.png",
         precio: 25000
     },
     {   
         id: "06",
         nombre: "Shirt Vulcane",
         Destacado: true,
-        imagen: "../image/remera2.png",
+        imagen: basePath + "remera2.png",
         precio: 28000
     },
     {   
         id: "07",
         nombre: "Shirt Mountain",
         Destacado: true,
-        imagen: "../image/remera3.png",
+        imagen: basePath + "remera3.png",
         precio: 22000
     },
 ]
